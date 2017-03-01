41ec6b2cbae197fc82f922f1711dffa80e2eb085
CORA Lukasz <B3J@dsone.3ds.com>
2017-03-01 22:28:15 +0100

    new files and updates

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyDYVyxR-YSPmZuaRugQwlw-gLjWB1eDgFc",
+    authDomain: "catch-of-the-day-74bf7.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-74bf7.firebaseio.com"
+});
+
+export default base;
--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,7 @@ import Order from './Order';
 import Inventory from './Inventory';
 import Fish from './Fish';
 import sampleFishes from '../sample-fishes.js';
+import base from '../base';
 
 class App extends React.Component {
 	constructor() {
@@ -11,11 +12,37 @@ class App extends React.Component {
 		this.addFish = this.addFish.bind(this);
 		this.loadSamples = this.loadSamples.bind(this);
 		this.addToOrder = this.addToOrder.bind(this);
+		this.updateFish = this.updateFish.bind(this);
 		//get initial state
 		this.state = {
 			fishes: {},
 			order: {}
 		};
+	}
+
+	// hook runs before app is rendered
+	componentWillMount() {
+		this.ref = base.syncState(`${this.props.params.storeId}/fishes`
+			, {
+			context: this,
+			state: 'fishes'
+		});
+
+		const localStorageRef = localStorage.getItem(`order-${this.props.params.storeId}`);
+
+		if ( localStorageRef ) {
+			this.setState({
+				order: JSON.parse( localStorageRef )
+			});
+		}
+	}
+
+	componentWillUnmount() {
+		base.removeBinding(this.ref);
+	}
+
+	componentWillUpdate(nextProps, nextState) {
+		localStorage.setItem( `order-${this.props.params.storeId}`, JSON.stringify(nextState.order) );
 	}
 
 	addFish(fish) {
@@ -27,6 +54,12 @@ class App extends React.Component {
 		// set state
 		this.setState({ fishes });
 	}
+
+	updateFish(key, updatedFish) {
+		const fishes = {...this.state.fishes};
+		fishes[key] = updatedFish;
+		this.setState({ fishes });
+	};
 
 	loadSamples() {
 		this.setState({
@@ -51,8 +84,17 @@ class App extends React.Component {
 						)}
 					</ul>
 				</div>
-				<Order fishes={this.state.fishes} order={this.state.order} />
-				<Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
+				<Order 
+					fishes={this.state.fishes} 
+					order={this.state.order} 
+					params={this.props.params}
+				/>
+				<Inventory 
+					addFish={this.addFish} 
+					loadSamples={this.loadSamples} 
+					fishes={this.state.fishes}
+					updateFish={this.updateFish}
+				/>
 			</div>
 		)
 	}
